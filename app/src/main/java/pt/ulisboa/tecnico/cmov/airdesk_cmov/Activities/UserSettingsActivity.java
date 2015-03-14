07a4dd18656ab629993309f577155cab6c92e684
Rui Pereira <ruijosemangas@gmail.com>
2015-03-14 17:33:47 +0000

    a lot of changes

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk_cmov/Activities/UserSettingsActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk_cmov/Activities/UserSettingsActivity.java
@@ -1,45 +1,37 @@
 package pt.ulisboa.tecnico.cmov.airdesk_cmov.Activities;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import pt.ulisboa.tecnico.cmov.airdesk_cmov.R;
 
-
-public class WorkSpacesActivity extends ActionBarActivity {
+public class UserSettingsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_spaces);
-
-        TextView text = (TextView)findViewById(R.id.textView4);
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("location");
-        text.setText(str);
+        setContentView(R.layout.activity_user_settings_acitivity);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_work_spaces, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_settings_acitivity, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(WorkSpacesActivity.this, UserSettingsActivity.class);
-            startActivity(intent);
             return true;
         }
 
