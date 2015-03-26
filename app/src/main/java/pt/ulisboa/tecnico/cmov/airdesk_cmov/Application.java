a50315bdcbe376150da724ce9dfc2f41023912f3
= <pbraz.93@gmail.com>
2015-03-26 10:27:52 +0000

    fixed dialog; created Application Owner

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk_cmov/Application.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk_cmov/Application.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.cmov.airdesk_cmov;
 
+import android.test.ApplicationTestCase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,16 +17,20 @@ import pt.ulisboa.tecnico.cmov.airdesk_cmov.Sessions.SessionManager;
 
 public class Application {
 
-    private static User owner = null;
+
     public static SessionManager session = null;
 
-    private List<Workspace> myWorkspaces;
+    private static ApplicationOwner owner = null;
 
     private static UsersDataSource userData = null;
 
-    public static final int MAX_APPLICATION_QUOTA = 20; //do something with this
+    //The Data source should not be called from here, should instead be called from the owner
     private static WorkspacesDataSource workspaceData = null;
+    public static final int MAX_APPLICATION_QUOTA = 20; //do something with this
 
+    //maybe move this inside the user, right now this is temporary because the foreign
+    // workspaces will work as dummy data
+    public static final List<Workspace> foreignWorkspaces = new ArrayList<Workspace>();
 
     public static void init(android.content.Context AppContext) {
         Application.setUsersDataSource(new UsersDataSource(AppContext));
@@ -53,11 +59,10 @@ public class Application {
         Application.workspaceData = workspacedb;
     }
 
-    public static User getOwner() { return Application.owner; }
+    public static ApplicationOwner getOwner() { return Application.owner; }
 
     public Application (User u) {
-        Application.owner = u;
-        this.myWorkspaces = new ArrayList<>();
+        Application.owner = (ApplicationOwner) u;
     }
 
     private static boolean checkUser(User u){
@@ -74,23 +79,9 @@ public class Application {
         User u = userData.get(email);
         if (u == null)throw new NotRegisteredException();
 
-        Application.owner = userData.get(email);
+        Application.owner = ApplicationOwner.fromUser(u);
 
     }
 
-    public static List<Workspace> getMyWorkspaces(){
-
-        List<Workspace> myWorkspaces = new ArrayList<>();
-        List<Workspace> allWorkspaces = workspaceData.getAll();
-
-        System.out.println(allWorkspaces.toString());
-
-        for(Workspace work : allWorkspaces){
-            if (work.getOwner().getEmail().equals(Application.getOwner().getEmail()))
-                myWorkspaces.add(work);
-        }
-
-        return myWorkspaces;
-    }
 
 }
